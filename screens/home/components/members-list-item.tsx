e04792f09b6514052c35e77514d897b34946d8cf
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-10-29 16:53:43 +0400

    Add home screen component and member items

---
+++ screens/home/components/members-list-item.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { Box, Heading, Link, Text } from '@chakra-ui/core';
+
+type MembersListItemProps = {
+  title: string;
+  designation: string;
+  company: string;
+  imageUrl: string;
+};
+
+export function MembersListItem(props: MembersListItemProps) {
+  const { company, designation, imageUrl, title } = props;
+
+  return (
+    <Link bg='gray.900'>
+      <Image className='member-grid-img' unsized src={ imageUrl } />
+      <Box px='10px' py='15px'>
+        <Heading color='gray.100' mb='7px' fontSize='18px'>{ title }</Heading>
+        <Text color='gray.500' fontSize='14px'>{ designation } @ <Text as='span' color='gray.100'>{ company }</Text></Text>
+      </Box>
+    </Link>
+  );
+}
