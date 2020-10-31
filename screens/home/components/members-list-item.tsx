39a542c232e63c5b8a8d9d8533939852f0522e9b
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-10-31 10:09:24 +0400

    Replace next/image with chakra image

--- screens/home/components/members-list-item.tsx
+++ screens/home/components/members-list-item.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import { Box, Heading, Link, Text } from '@chakra-ui/core';
+import { Box, Heading, Image, Link, Text } from '@chakra-ui/core';
 
 type MembersListItemProps = {
   title: string;
@@ -12,8 +11,8 @@ export function MembersListItem(props: MembersListItemProps) {
   const { company, designation, imageUrl, title } = props;
 
   return (
-    <Link bg='gray.900' rounded='10px' _hover={{ bg: 'gray.800'}}>
-      <Image className='member-grid-img' unsized src={ imageUrl } />
+    <Link bg='gray.900' rounded='10px' _hover={ { bg: 'gray.800' } }>
+      <Image w='100%' borderRadius={ '10px 10px 0 0' } src={ imageUrl } />
       <Box px='10px' py='15px'>
         <Heading color='gray.100' mb='7px' fontSize='18px'>{ title }</Heading>
         <Text color='gray.500' fontSize='14px'>{ designation } @ <Text as='span' color='gray.100'>{ company }</Text></Text>
