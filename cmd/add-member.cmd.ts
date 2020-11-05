b457df0102e1f097b29a8c2f3f7d65c9674808ef
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-11-06 01:07:30 +0400

    Add members

--- cmd/add-member.cmd.ts
+++ cmd/add-member.cmd.ts
@@ -1,11 +1,9 @@
-import fs from "fs"
-import path from "path"
-import fetch from "node-fetch"
-import fileType from "file-type"
-import inquirer from "inquirer"
-import {
-  Command
-} from "./index";
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+import fileType from 'file-type';
+import inquirer from 'inquirer';
+import { Command } from './index';
 
 const avatarsDir = path.join(process.cwd(), './public/avatars');
 const membersDir = path.join(process.cwd(), './data/members');
@@ -23,11 +21,11 @@ async function downloadAndSaveImg(answer: Answer) {
     const response = await fetch(answer.imageUrl);
     const buffer = await response.buffer();
     const fileDetails = await fileType.fromBuffer(buffer);
-    const fileName = `${answer.username}.${fileDetails.ext}`;
+    const fileName = `${ answer.username }.${ fileDetails.ext }`;
 
-    fs.writeFileSync(path.join(avatarsDir, fileName), buffer)
+    fs.writeFileSync(path.join(avatarsDir, fileName), buffer);
+
     return fileName;
-
   } catch (e) {
     return false;
   }
@@ -35,88 +33,86 @@ async function downloadAndSaveImg(answer: Answer) {
 
 function validateMinLength(input: string) {
   if (input.length < 4) {
-    return "length must be at least 4";
+    return 'length must be at least 4';
   } else {
     return true;
   }
 }
 
-async function validateUsername(username: Answer["username"]) {
+async function validateUsername(username: Answer['username']) {
   if (username.length < 4) {
-    return "length must be at least 4";
+    return 'length must be at least 4';
   } else {
-
     const memberDir = path.join(membersDir, username);
 
     if (!fs.existsSync(memberDir)) {
       fs.mkdirSync(memberDir);
       return true;
     } else {
-      return "username already exist";
+      return 'username already exist';
     }
   }
 }
 
 export const AddMemberCommand: Command = {
-  signature: "members.add",
-  title: "Add new member",
-  description: "Adding details of the new member",
-  run: async (): Promise < void > => {
+  signature: 'members.add',
+  title: 'Add new member',
+  description: 'Adding details of the new member',
+  run: async (): Promise<void> => {
     const answer: Answer = await inquirer
-      .prompt([{
-          name: "username",
-          type: "input",
-          message: "What is your username?",
-          validate: validateUsername,
+      .prompt([
+        {
+          name: 'title',
+          type: 'input',
+          message: 'Full name of the user?',
+          validate: validateMinLength
         },
         {
-          name: "title",
-          type: "input",
-          message: "What is your title (full name)?",
-          validate: validateMinLength,
+          name: 'username',
+          type: 'input',
+          message: 'Username of the user?',
+          validate: validateUsername
         },
         {
-          name: "designation",
-          type: "input",
-          message: "What is your designation?",
-          validate: validateMinLength,
+          name: 'designation',
+          type: 'input',
+          message: 'Designation of the user?',
+          validate: validateMinLength
         },
         {
-          name: "company",
-          type: "input",
-          message: "What is your company name?",
-          validate: validateMinLength,
+          name: 'company',
+          type: 'input',
+          message: 'Company at which this user works?',
+          validate: validateMinLength
         },
         {
-          name: "imageUrl",
-          type: "input",
-          message: "What is your imageUrl?",
-        },
-      ])
+          name: 'imageUrl',
+          type: 'input',
+          message: 'URL to take the image from?'
+        }
+      ]);
 
     const memberDir = path.join(membersDir, answer.username);
 
     const fileName = await downloadAndSaveImg(answer);
-
     if (!fileName) {
-      console.log("\n\nCould not save image..!\nExiting..!\n\n")
-      fs.rmdirSync(memberDir)
-      process.kill(process.pid, 'SIGINT')
+      console.log('\n\nCould not save image..!\nExiting..!\n\n');
+      fs.rmdirSync(memberDir);
+      process.kill(process.pid, 'SIGINT');
     }
 
     const userDetails = {
       ...answer,
-      imageUrl: `/avatars/${fileName}`,
-      joinDate: new Date().toISOString(),
+      imageUrl: `/avatars/${ fileName }`,
+      joinDate: new Date().toISOString()
     };
 
     // profile
     fs.writeFileSync(
-      path.join(memberDir, "profile.json"),
+      path.join(memberDir, 'profile.json'),
       JSON.stringify(userDetails, null, 4));
 
     // bio.md
-    fs.writeFileSync(path.join(memberDir, "bio.md"), "");
-
-  },
+    fs.writeFileSync(path.join(memberDir, 'bio.md'), '');
+  }
 };
