c810afa675f02085a5269582dc260535e7eeef03
Muhammed Abidh <abidh.k@appsintegra.com>
2020-11-05 21:39:10 +0400

    review comments addressed

--- cmd/add-member.cmd.ts
+++ cmd/add-member.cmd.ts
@@ -1,11 +1,14 @@
 import fs from "fs"
+import path from "path"
 import fetch from "node-fetch"
 import fileType from "file-type"
 import inquirer from "inquirer"
-import { Command } from "./index";
+import {
+  Command
+} from "./index";
 
-const IMAGE_DIR = "./public/avatars";
-const MEMBERS_DIR = "./data/members";
+const avatarsDir = path.join(process.cwd(), './public/avatars');
+const membersDir = path.join(process.cwd(), './data/members');
 
 type Answer = {
   username: string;
@@ -22,13 +25,11 @@ async function downloadAndSaveImg(answer: Answer) {
     const fileDetails = await fileType.fromBuffer(buffer);
     const fileName = `${answer.username}.${fileDetails.ext}`;
 
-    fs.writeFile(`${IMAGE_DIR}/${fileName}`, buffer, () => {
-      console.log("Image saved!");
-    });
+    fs.writeFileSync(path.join(avatarsDir, fileName), buffer)
+    return fileName;
 
-    return `/avatars/${fileName}`;
   } catch (e) {
-    console.log("Something went wrong, couldn't save image;", e);
+    return false;
   }
 }
 
@@ -44,7 +45,7 @@ async function validateUsername(username: Answer["username"]) {
   if (username.length < 4) {
     return "length must be at least 4";
   } else {
-    const directory = `${MEMBERS_DIR}/${username}`;
+    const directory = `${membersDir}/${username}`;
 
     if (!fs.existsSync(directory)) {
       fs.mkdirSync(directory);
@@ -55,71 +56,66 @@ async function validateUsername(username: Answer["username"]) {
   }
 }
 
-async function addMember() {
-  await inquirer
-    .prompt([
-      {
-        name: "username",
-        type: "input",
-        message: "What is your username?",
-        validate: validateUsername,
-      },
-      {
-        name: "title",
-        type: "input",
-        message: "What is your title (full name)?",
-        validate: validateMinLength,
-      },
-      {
-        name: "designation",
-        type: "input",
-        message: "What is your designation?",
-        validate: validateMinLength,
-      },
-      {
-        name: "company",
-        type: "input",
-        message: "What is your company name?",
-        validate: validateMinLength,
-      },
-      {
-        name: "imageUrl",
-        type: "input",
-        message: "What is your imageUrl?",
-      },
-    ])
-    .then(async (answer: Answer) => {
-      const directory = `${MEMBERS_DIR}/${answer.username}`;
-
-      const imgUrl = await downloadAndSaveImg(answer);
-
-      const userDetails = {
-        ...answer,
-        imageUrl: imgUrl,
-        joinDate: new Date().toISOString(),
-      };
-
-      // profile
-      fs.writeFile(
-        `${directory}/profile.json`,
-        JSON.stringify(userDetails),
-        function (err: Error) {
-          if (err) throw err;
-        }
-      );
-
-      // bio.md
-      fs.writeFile(`${directory}/bio.md`, "", function (err: Error) {
-        if (err) throw err;
-      });
-    });
-}
-
 export const AddMemberCommand: Command = {
   signature: "members.add",
   title: "Add new member",
   description: "Adding details of the new member",
-  run: async (): Promise<void> => {
-    await addMember();
+  run: async (): Promise < void > => {
+    const answer: Answer = await inquirer
+      .prompt([{
+          name: "username",
+          type: "input",
+          message: "What is your username?",
+          validate: validateUsername,
+        },
+        {
+          name: "title",
+          type: "input",
+          message: "What is your title (full name)?",
+          validate: validateMinLength,
+        },
+        {
+          name: "designation",
+          type: "input",
+          message: "What is your designation?",
+          validate: validateMinLength,
+        },
+        {
+          name: "company",
+          type: "input",
+          message: "What is your company name?",
+          validate: validateMinLength,
+        },
+        {
+          name: "imageUrl",
+          type: "input",
+          message: "What is your imageUrl?",
+        },
+      ])
+
+    const memberDir = path.join(membersDir, answer.username);
+
+    const fileName = await downloadAndSaveImg(answer);
+
+    if (!fileName) {
+      console.log("\n\nCould not save image..!\nExiting..!\n\n")
+      fs.rmdirSync(memberDir)
+      process.kill(process.pid, 'SIGINT')
+    }
+
+    const userDetails = {
+      ...answer,
+      imageUrl: `/avatars/${fileName}`,
+      joinDate: new Date().toISOString(),
+    };
+
+    // profile
+    fs.writeFileSync(
+      path.join(memberDir, "profile.json"),
+      JSON.stringify(userDetails, null, 4));
+
+    // bio.md
+    fs.writeFileSync(path.join(memberDir, "bio.md"), "");
+
   },
 };
